21593d10335d4ded4283ba8da1bd235f3f879b9a
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-03-28 09:42:42 +0500

    input mask add

--- src/app/sections/Utils.jsx
+++ src/app/sections/Utils.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+"use client";
+import Image from "next/image";
+import React from "react";
+import InputMask from "react-input-mask";
+
+import Util from "../../assets/img/utils.png";
+import Daraxt from "../../assets/img/daraxt.png";
 
 function Utils(props) {
-    return (
-        <div>
-            Utils
+  return (
+    <section>
+      <div className="flex flex-col">
+        <h2>Utilitalar</h2>
+        <div className="flex">
+          <div className="img1">
+            <Image src={Util} alt="img" />
+          </div>
+          <div className="img1">
+            <Image src={Daraxt} alt="img" />
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex flex-col justify-start">
+        <h2>Ekspress to`lov</h2>
+        <form>
+          <div className="inp">
+            <label htmlFor="tel">Telefon raqami</label>
+            <InputMask mask="+\9\98 (99) 999-99-99" maskChar="_">
+              {(inputProps) => (
+                <input
+                className=""
+                  {...inputProps}
+                  type="text"
+                  name="tel"
+                  id="tel"
+                  placeholder="+998 (_) ___-__-__"
+                />
+              )}
+            </InputMask>
+          </div>
+          <div className="inp">
+            <label htmlFor="sum">To`lov miqdori</label>
+            <InputMask
+        mask="9 999 999 999"
+        maskChar="_"
+        placeholder="10 000"
+        value={props.amount}
+        onChange={props.onChange}
+      >
+        {(inputProps) => <input {...inputProps} />}
+      </InputMask>
+          </div>
+        </form>
+      </div>
+      Utils
+    </section>
+  );
 }
 
 export default Utils;
--- src/app/sections/Valyuta.jsx
+++ src/app/sections/Valyuta.jsx
@@ -4,44 +4,66 @@ import Rasm from "../../assets/img/kurslar.png";
 import ValIcon from "@/assets/icon/ValIcon";
 import Transform from "@/assets/icon/Transform";
 
-
-
 function Valyuta(props) {
   return (
     <section>
       <div className="container">
         <div className="flex flex-col">
           <div className="flex justify-between items-center mb-5">
-            <h3 className="md:text-[24px] text-[18px] font-bold lg:leading-[33px] leading-[24px] text-[#17171C] exchange_title">Valyuta kursi</h3>
+            <h3 className="md:text-[24px] text-[18px] font-bold lg:leading-[33px] leading-[24px] text-[#17171C] exchange_title">
+              Valyuta kursi
+            </h3>
             <div class="flex justify-between items-center">
               <span class="text-[#A5AAB4] leading-[21px] based_span hidden md:block mr-2">
                 Asosida:
               </span>
-              <ValIcon/>
+              <ValIcon />
               <h3 class="lg:text-[13px] text-[10px] font-semibold lg:leading-[13px] leading-[10px] text-[#17171C] ml-2 w-[200px] central_bank">
                 O`zbekiston Respublikasi Markaziy banki
               </h3>
             </div>
           </div>
           <div className=" grid lg:grid-cols-2 gap-5 grid-cols-1 items-stretch">
-            
-            <div className="bg-slate-50">
-                <div className="flex ">
-                    <input className=" focus:outline-none lg:text-[34px] text-[18px] w-full exchange_input leading-[49px] font-bold text-[#A5AAB4] bg-transparent" placeholder="0" type="text" name="uzb" id="uzb" />
-                    <label className="text-[20px] leading-[27px] font-bold ml-[10px]" htmlFor="uzb">uzb</label>
-                </div>
-                    <div className="mx-auto ">
-                    <span className="p-2 rounded-lg bg-[#E6E9EF] mx-6 lg:mt-3 mt-5">
-                    
-                    <Transform/>
-                    </span>
-                    </div>
-                <div className="flex">
-                    
-                    <input className=" focus:outline-none lg:text-[34px] text-[18px] w-full exchange_input leading-[49px] font-bold text-[#A5AAB4] bg-transparent" placeholder="0.00" type="text" name="usd" id="usd" />
-                    <label className="text-[20px] leading-[27px] font-bold ml-[10px]" htmlFor="usd">usd</label>
-                </div>
+            <div className="bg-slate-50 w-full">
+              <div className="flex border-b border-solid border-gray-300">
+                <input
+                  className="focus:outline-none lg:text-[34px] text-[18px] w-full exchange_input leading-[49px] font-bold text-[#A5AAB4] bg-transparent"
+                  placeholder="0"
+                  type="text"
+                  name="uzb"
+                  id="uzb"
+                />
+                <label
+                  className="text-[20px] leading-[27px] font-bold ml-[10px]"
+                  htmlFor="uzb"
+                >
+                  UZB
+                </label>
+              </div>
+
+              <div className="flex justify-center items-start py-5">
+                <span className="block mx-auto p-3 bg-slate-300 rounded-xl text-center">
+                  <Transform />
+                </span>
+              </div>
+
+              <div className="flex border-b border-solid border-gray-300">
+                <input
+                  className="focus:outline-none lg:text-[34px] text-[18px] w-full exchange_input leading-[49px] font-bold text-[#A5AAB4] bg-transparent"
+                  placeholder="0.00"
+                  type="text"
+                  name="usd"
+                  id="usd"
+                />
+                <label
+                  className="text-[20px] leading-[27px] font-bold ml-[10px]"
+                  htmlFor="usd"
+                >
+                  USD
+                </label>
+              </div>
             </div>
+
             <div className="img">
               <Image src={Rasm} alt="img" />
             </div>
