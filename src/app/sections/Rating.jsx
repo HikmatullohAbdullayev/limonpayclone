cccee8b6cb7e2c91f4693593283e8b6520c029b6
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-03-30 14:01:11 +0500

    rating section add

--- src/app/sections/Rating.jsx
+++ src/app/sections/Rating.jsx
@@ -1,44 +1,75 @@
-import React from "react";
+///
+import React from 'react';
+import Image from "next/image";
+import appLogo from "../../assets/img/applelogo.png"
+import googleLogo from "../../assets/img/googleplaylogo.png"
+import Ster from "../../assets/img/stars.png"
+
+///
 
 function Rating(props) {
-  return (
-    <div>
-      <div className="container mx-auto mt-20 ">
-        <div className="flex justify-between gap-5 mb-10 ">
-          <div className="flex w-[50%] justify-between rounded-lg border bg-slate-100  ">
-            <div className="my-10 ml-10">
-              <i className=" fa-brands fa-apple mb-20 text-[25px]"></i>
-              <p>Reyting</p>
-              <h4 className="text-2xl font-bold">App store</h4>
+    return (
+        <div>
+             <div className="container  mt-20 ">
+      <div className="flex justify-between gap-5 mb-10 ">
+        <div className="flex w-[500px] justify-between rounded-lg   bg-slate-100  ">
+          <div className="py-10 px-10 w-full flex flex-col gap-8 justify-between ">
+            <div className="flex justify-between items-center gap-4 w-full">
+            <div className="img">
+            <Image className="inline-block" src={appLogo} alt="apple"/>
             </div>
-            <div className="my-10 mr-10">
-              <div className="flex mb-20"></div>
-              <div>
-                <h1 className="text-4xl font-bold text-yellow-500">
-                  4.9 <span className="text-xs text-black ">5 Dan</span>
-                </h1>
-              </div>
+            <div className="star">
+                <Image className="inline-block" src={Ster} alt="star"/>
+            </div>
+            </div>
+            <div className="flex justify-between w-full">
+            <div className="">
+            <p className="text-gray-600">Reyting</p>
+            <p className="text-2xl font-bold">App store</p>
+            </div>
+          <div className="">
+           
+            <div>
+              <p className="text-xl font-bold text-yellow-500">
+                <span className=" text-[58px]">4.9</span> 5 Dan
+              </p>
             </div>
           </div>
-          <div className="flex w-[50%] justify-between rounded-lg border bg-slate-100  ">
-            <div className="my-10 ml-10">
-              <i className=" fa-brands fa-google-play mb-20 text-[25px]"></i>
-              <p>Reyting</p>
-              <h4 className="text-2xl font-bold">Google play</h4>
-            </div>
-            <div className="my-10 mr-10">
-              <div className="flex mb-20"></div>
-              <div>
-                <h1 className="text-4xl font-bold text-yellow-500">
-                  4.6 <span className="text-xs text-black ">5 Dan</span>
-                </h1>
-              </div>
             </div>
           </div>
         </div>
+        <div className="flex w-[500px] justify-between rounded-lg   bg-slate-100  ">
+          <div className="py-10 px-10 w-full flex flex-col gap-8 justify-between ">
+            <div className="flex justify-between items-center gap-4 w-full">
+            <div className="img">
+            <Image className="inline-block" src={googleLogo} alt="apple"/>
+            </div>
+            <div className="star">
+                <Image className="inline-block" src={Ster} alt="star"/>
+            </div>
+            </div>
+            <div className="flex justify-between w-full">
+            <div className="">
+            <p className="text-gray-600">Reyting</p>
+            <p className="text-2xl font-bold">Google play</p>
+            </div>
+          <div className="">
+           
+            <div>
+              <p className="text-xl font-bold text-yellow-500">
+                <span className=" text-[58px]">4.9</span> 5 Dan
+              </p>
+            </div>
+          </div>
+            </div>
+          </div>
+        </div>
+        
+        
       </div>
     </div>
-  );
+        </div>
+    );
 }
 
 export default Rating;
