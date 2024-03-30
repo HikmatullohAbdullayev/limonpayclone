42ea36bdabb3bb90f176c0d39f66404cbcb26d7b
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-03-30 13:05:02 +0500

    app section done

--- src/app/sections/AppStore.jsx
+++ src/app/sections/AppStore.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import App from "../../assets/img/applestore.png";
+import Google from "../../assets/img/googleplay.png";
+import phone from "../../assets/img/qurilmalar.png";
 
 function AppStore(props) {
-    return (
-        <div>
-            AppStore
+  return (
+    <section className="container flex justify-between mt-[200px]">
+      <div className=" rounded-2xl w-full bg-yellow-500 px-5 relative ">
+        <div className="flex flex-col justify-between">
+          <p className="mt-10 font-bold text-3xl text-black">
+            Qurilmangiz uchun ilovani yuklab oling
+          </p>
+          <p className="pt-5 text-xl">
+            Dastur iOS, Android tizimlarini qo`llab-quvvatlaydi.
+          </p>
+          <div className="flex gap-8 my-8">
+            <div>
+              <Image className="rounded-lg" src={App} alt="img" />
+            </div>
+            <div>
+              <Image className="rounded-lg" src={Google} alt="img" />
+            </div>
+          </div>
         </div>
-    );
+        <div className="w-[400px] absolute top-[-143px] right-0 ">
+        <Image className="" src={phone} alt="img" />
+      </div>
+      </div>
+     
+    </section>
+  );
 }
 
 export default AppStore;
--- src/app/sections/Rating.jsx
+++ src/app/sections/Rating.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
 
 function Rating(props) {
-    return (
-        <div>
-            Rating
+  return (
+    <div>
+      <div className="container mx-auto mt-20 ">
+        <div className="flex justify-between gap-5 mb-10 ">
+          <div className="flex w-[50%] justify-between rounded-lg border bg-slate-100  ">
+            <div className="my-10 ml-10">
+              <i className=" fa-brands fa-apple mb-20 text-[25px]"></i>
+              <p>Reyting</p>
+              <h4 className="text-2xl font-bold">App store</h4>
+            </div>
+            <div className="my-10 mr-10">
+              <div className="flex mb-20"></div>
+              <div>
+                <h1 className="text-4xl font-bold text-yellow-500">
+                  4.9 <span className="text-xs text-black ">5 Dan</span>
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-[50%] justify-between rounded-lg border bg-slate-100  ">
+            <div className="my-10 ml-10">
+              <i className=" fa-brands fa-google-play mb-20 text-[25px]"></i>
+              <p>Reyting</p>
+              <h4 className="text-2xl font-bold">Google play</h4>
+            </div>
+            <div className="my-10 mr-10">
+              <div className="flex mb-20"></div>
+              <div>
+                <h1 className="text-4xl font-bold text-yellow-500">
+                  4.6 <span className="text-xs text-black ">5 Dan</span>
+                </h1>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Rating;
